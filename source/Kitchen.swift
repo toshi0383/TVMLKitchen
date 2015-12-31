5866cad24fd2edc4696a7005b4700445898c534b
toshi0383 <t.suzuki326@gmail.com>
2015-12-31 22:36:53 +0900

    [clean]

--- source/Kitchen.swift
+++ source/Kitchen.swift
@@ -6,7 +6,6 @@
 //  Copyright © 2015 toshi0383. All rights reserved.
 //
 
-import Foundation
 import TVMLKit
 
 public typealias JavaScriptEvaluationHandler = (TVApplicationController, JSContext) -> Void
