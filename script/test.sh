ddfa946af26ae85adeb4eb9a287151d901328a29
toshi0383 <t.suzuki326@gmail.com>
2016-01-12 12:06:06 +0900

    Add swiftlint test phase to test.sh

--- script/test.sh
+++ script/test.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+swiftlint lint --strict
+if [ $? -ne 0 ];then
+    echo SwiftLint validation error !
+    exit 1
+fi
 xcodebuild "-project" "TVMLKitchen.xcodeproj" "-scheme" "TVMLKitchen" "clean" "test" "-destination" "platform=tvOS Simulator,name=Apple TV 1080p,OS=latest" | xcpretty "--color"
 xcodebuild "-project" "TVMLKitchen.xcodeproj" "-scheme" "SampleRecipeUITests" "test" "-destination" "platform=tvOS Simulator,name=Apple TV 1080p,OS=latest" | xcpretty "--color"
