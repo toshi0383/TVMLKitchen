3376906dae2d5b131d51e9676ff014d067dd4eb5
toshi0383 <t.suzuki326@gmail.com>
2016-01-25 14:46:47 +0900

    Fix test.sh

--- script/test.sh
+++ script/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-swiftlint lint --strict
-if [ $? -ne 0 ];then
-    echo SwiftLint validation error !
-    exit 1
-fi
+set -o pipefail
 xcodebuild "-project" "TVMLKitchen.xcodeproj" "-scheme" "TVMLKitchen" "clean" "test" "-destination" "platform=tvOS Simulator,name=Apple TV 1080p,OS=latest" | xcpretty "--color"
+st=$?
+if [ $st -ne 0 ];then
+	exit $st
+fi
 xcodebuild "-project" "TVMLKitchen.xcodeproj" "-scheme" "SampleRecipeUITests" "test" "-destination" "platform=tvOS Simulator,name=Apple TV 1080p,OS=latest" | xcpretty "--color"
