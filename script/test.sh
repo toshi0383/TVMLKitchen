5a3978c836acb42ed1c6bdcef675c97ec9c81c28
toshi0383 <t.suzuki326@gmail.com>
2016-02-03 22:07:29 +0900

    Set exit code

--- script/test.sh
+++ script/test.sh
@@ -6,4 +6,6 @@ set -o pipefail
 #	exit $st
 #fi
 xcodebuild "-project" "TVMLKitchen.xcodeproj" "-scheme" "SampleRecipeUITests" "test" "-destination" "platform=tvOS Simulator,name=Apple TV 1080p,OS=9.1" | xcpretty "--color"
+st=$?
 sleep 10
+exit $st
