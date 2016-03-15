e6624f51e06e90b799d058d23a140006c854de0a
Stephen Radford <steve228uk@gmail.com>
2016-03-15 10:27:15 +0000

    Add descriptive alert recipe and fix typo

--- source/recipes/AlertRecipe.swift
+++ source/recipes/AlertRecipe.swift
@@ -18,13 +18,16 @@ public struct AlertButton {
     
 }
 
-public struct AlertRecipe: RecipeType {
+public class AlertRecipe: RecipeType {
     
     public let theme = DefaultTheme()
     public let presentationType = PresentationType.Modal
     public let title: String
     public let description: String
     public let buttons: [AlertButton]
+    var templateFile: String {
+        return "AlertRecipe"
+    }
     
     public init(title: String, description: String, buttons: [AlertButton] = []) {
         self.title = title
@@ -43,7 +46,7 @@ public struct AlertRecipe: RecipeType {
     private var buttonString: String {
         let mapped: [String] = buttons.map {
             var string = ($0.actionID != nil) ? "<button actionID=\"\($0.actionID!)\">" : "<button>"
-            string += "<title>\($0.title)</title>"
+            string += "<text>\($0.title)</text>"
             string += "</button>"
             return string
         }
@@ -52,7 +55,7 @@ public struct AlertRecipe: RecipeType {
     
     public var template: String {
         var xml = ""
-        if let url = NSBundle.mainBundle().URLForResource("AlertRecipe", withExtension: "xml") {
+        if let url = NSBundle.mainBundle().URLForResource(templateFile, withExtension: "xml") {
             xml = try! String(contentsOfURL: url)
             xml = xml.stringByReplacingOccurrencesOfString("{{TITLE}}", withString: title)
             xml = xml.stringByReplacingOccurrencesOfString("{{DESCRIPTION}}", withString: description)
