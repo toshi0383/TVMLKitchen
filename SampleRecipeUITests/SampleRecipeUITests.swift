59a340e3285cb0a1af3aa4a7e7978aa82e369b6f
toshi0383 <t.suzuki326@gmail.com>
2016-03-19 22:34:34 +0900

    Fix storyboard and UITest case

--- SampleRecipeUITests/SampleRecipeUITests.swift
+++ SampleRecipeUITests/SampleRecipeUITests.swift
@@ -16,35 +16,36 @@ class SampleRecipeUITests: XCTestCase {
         XCUIApplication().launch()
     }
 
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCUIRemote.sharedRemote().pressButton(.Up)
-        XCUIRemote.sharedRemote().pressButton(.Down)
-        XCUIRemote.sharedRemote().pressButton(.Right)
-        XCUIRemote.sharedRemote().pressButton(.Right)
-        XCUIRemote.sharedRemote().pressButton(.Right)
-        XCUIRemote.sharedRemote().pressButton(.Down)
-        XCUIRemote.sharedRemote().pressButton(.Up)
-        XCUIRemote.sharedRemote().pressButton(.Left)
-        XCUIRemote.sharedRemote().pressButton(.Left)
-        XCUIRemote.sharedRemote().pressButton(.Up)
-        XCUIRemote.sharedRemote().pressButton(.Down)
-        XCUIRemote.sharedRemote().pressButton(.Down)
-
+    func testUI() {
         let app = XCUIApplication()
-        XCUIRemote.sharedRemote().pressButton(.Select)
-        XCTAssert(app.buttons["Template.xml.js"].hasFocus)
-        XCUIRemote.sharedRemote().pressButton(.Select)
-        XCUIRemote.sharedRemote().pressButton(.Menu)
-        sleep(1)
-        XCTAssert(app.buttons["Template.xml.js"].hasFocus)
         XCUIRemote.sharedRemote().pressButton(.Right)
         XCUIRemote.sharedRemote().pressButton(.Right)
-        XCTAssert(app.buttons["XMLString"].hasFocus)
+        XCUIRemote.sharedRemote().pressButton(.Select)
+        let catalog = app.buttons["Catalog.xml"]
+        XCTAssert(catalog.hasFocus)
         XCUIRemote.sharedRemote().pressButton(.Select)
         sleep(1)
         XCUIRemote.sharedRemote().pressButton(.Menu)
         sleep(1)
+        XCUIRemote.sharedRemote().pressButton(.Right)
+        let xmlString = app.buttons["XMLString"]
+        XCTAssert(xmlString.hasFocus)
+        XCUIRemote.sharedRemote().pressButton(.Select)
+        sleep(1)
+        XCUIRemote.sharedRemote().pressButton(.Menu)
+        sleep(1)
+        XCUIRemote.sharedRemote().pressButton(.Down)
+        XCTAssert(app.buttons["Custom Theme"].hasFocus)
+        XCUIRemote.sharedRemote().pressButton(.Select)
+        sleep(1)
+        XCUIRemote.sharedRemote().pressButton(.Menu)
+        sleep(1)
+        XCUIRemote.sharedRemote().pressButton(.Down)
+        XCUIRemote.sharedRemote().pressButton(.Down)
+        XCTAssert(app.buttons["URL"].hasFocus)
+        XCUIRemote.sharedRemote().pressButton(.Select)
+        sleep(1)
+        XCUIRemote.sharedRemote().pressButton(.Menu)
+
     }
 }
