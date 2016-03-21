79b77d60619b413d11d03f6bd00265a8916223c1
toshi0383 <t.suzuki326@gmail.com>
2016-03-21 21:47:24 +0900

    Clean directories and classes

--- source/KitchenTabBar.swift
+++ source/KitchenTabBar.swift
@@ -6,6 +6,18 @@
 //  Copyright © 2016 toshi0383. All rights reserved.
 //
 
+public protocol TabItem {
+
+    /// The title that will be displayed on the tab bar.
+    var title: String { get }
+
+    /**
+     This handler will be called whenever the focus changes to it.
+     */
+    func handler()
+
+}
+
 public struct KitchenTabBar {
 
     /// The shared instance of the tab bar.
