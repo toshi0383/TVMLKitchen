eef1d2747779774e3e43002a14d098a75284e23c
Anthony Castelli <neueanthony@me.com>
2016-03-21 15:55:53 -0700

    Add support for Swift 2.2

--- Sources/Kitchen.swift
+++ Sources/Kitchen.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2015 toshi0383. All rights reserved.
 //
 
-@exported import TVMLKit
+import TVMLKit
 
 public typealias JavaScriptEvaluationHandler = (TVApplicationController, JSContext) -> Void
 public typealias KitchenErrorHandler = NSError -> Void
