6a299e4ce56a2b0ce3596140dd22ecf1efc5bbfb
toshi0383 <t.suzuki326@gmail.com>
2016-09-14 17:19:37 +0900

    Update Sample Code

--- NativeBaseSample/ViewController.swift
+++ NativeBaseSample/ViewController.swift
@@ -13,46 +13,52 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.view.backgroundColor = .blackColor()
+        view.backgroundColor = .blackColor()
     }
 
     @IBAction func urlString(sender: AnyObject!) {
         let appdelegateWindow = (UIApplication.sharedApplication().delegate as! AppDelegate).window!
-        Kitchen.window.alpha = 0.0
         Kitchen.serve(
             urlString: Sample.tvmlUrl,
             redirectWindow: appdelegateWindow,
+
+            // - Note: This is what Kitchen would do when animatedWindowTransition is true.
             kitchenWindowWillBecomeVisible: {
-                UIView.animateWithDuration(0.3) {
-                    Kitchen.window.alpha = 1.0
-                    appdelegateWindow.alpha = 0.0
-                }
+                Kitchen.window.alpha = 0.0
+                UIView.animateWithDuration(
+                    0.3,
+                    animations: {
+                        Kitchen.window.alpha = 1.0
+                    },
+                    completion: {
+                        _ in
+                        appdelegateWindow.alpha = 0.0
+                    }
+                )
             },
-            didRedirectToWindow: {
-                UIView.animateWithDuration(0.3) {
-                    Kitchen.window.alpha = 0.0
-                    appdelegateWindow.alpha = 1.0
-                }
+            willRedirectToWindow: {
+                appdelegateWindow.alpha = 0.0
+                UIView.animateWithDuration(
+                    0.3,
+                    animations: {
+                        appdelegateWindow.alpha = 1.0
+                    },
+                    completion: {
+                        _ in
+                        Kitchen.window.alpha = 0.0
+                    }
+                )
             }
         )
     }
     @IBAction func xmlString(sender: AnyObject!) {
         let appdelegateWindow = (UIApplication.sharedApplication().delegate as! AppDelegate).window!
-        Kitchen.window.alpha = 0.0
+        Kitchen.window.alpha = 1.0
         Kitchen.serve(
             xmlString: Sample.tvmlString,
             redirectWindow: appdelegateWindow,
-            kitchenWindowWillBecomeVisible: {
-                UIView.animateWithDuration(0.3) {
-                    Kitchen.window.alpha = 1.0
-                    appdelegateWindow.alpha = 0.0
-                }
-            },
-            didRedirectToWindow: {
-                UIView.animateWithDuration(0.3) {
-                    Kitchen.window.alpha = 0.0
-                    appdelegateWindow.alpha = 1.0
-                }
-            }
+            animatedWindowTransition: true
         )
     }
 }
