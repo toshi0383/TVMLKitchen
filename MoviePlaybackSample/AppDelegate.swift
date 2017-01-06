6e645d099057d95f18eb2f8499f6499269c4eecc
Wei Xia <xiawei@us.ibm.com>
2017-01-06 15:39:20 -0500

    delete storyboard

--- MoviePlaybackSample/AppDelegate.swift
+++ MoviePlaybackSample/AppDelegate.swift
@@ -46,14 +46,13 @@ func startPlayback(url: NSURL) {
         dispatch_async(dispatch_get_main_queue())
         {
             Kitchen.navigationController.pushViewController(vc, animated: true)
-            vc.player?.play()
         }
     }
 }
 
 private func xmlString() -> String {
     
-    let data = NSData(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("sample", ofType: "xml")!))
+    let data = NSData(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Sample", ofType: "xml")!))
     
     return String(data)
 }
@@ -64,17 +63,43 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     
+    func applicationWillResignActive(application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+    
+    func applicationDidEnterBackground(application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+    
+    func applicationWillEnterForeground(application: UIApplication) {
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    }
+    
+    func applicationDidBecomeActive(application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+    
+    func applicationWillTerminate(application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
+        //        Kitchen.prepare(Cookbook(launchOptions: launchOptions))
         
         let cookbook = Cookbook(launchOptions: launchOptions)
         cookbook.actionIDHandler = {
             actionID in
+            // actionID can be any string.
+            // In this sample, we're expecting comma separated string.
             if let action = Action(string: actionID) {
                 action.run()
             }
         }
         
+        // This callback is triggered by `play/pause` button.
         cookbook.playActionIDHandler = {
             playActionID in
             if let action = Action(string: playActionID) {
